30c3a785800eeda29f2cdf94f3e50b04919ddbab
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-28 18:33:58 -0500

    Remove bootstrap and add and configure react-toolbox to style up the application.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 
+import theme from './toolbox/theme';
 import App from './components/App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
