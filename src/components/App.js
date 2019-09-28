26eeb741069470f443841ccb7a15e697664cf960
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-28 18:41:50 -0500

    Add all the components to the UI and style them up.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React, {Component} from 'react';
+
+import {getQueryParams} from '../utils/common';
+import Login from './Login';
+import Main from './Main';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Button>Hi!</Button>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    const params = getQueryParams();
+    this.state = {token: params.token};
+  }
+
+  isLoggedIn() {
+    return !!this.state.toke;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.isLoggedIn() ? <Main token={this.state.token} /> : <Login />}
+      </div>
+    );
+  }
 }
 
 export default App;
